677254cb630d613f3c667022648e5958efc5d8a8
ch5766 <choiwy2001@naver.com>
2014-09-11 16:04:16 +0900

    choi3
    zz.z.z.z

--- sketch.js
+++ sketch.js
@@ -2,11 +2,9 @@ function setup() {
   createCanvas(640, 480);
   background(128, 0, 64);
   noFill();
-
-  stroke(255, 0, 0);
-
-  for (var x = 1; x <=8; x++)
-{
-  ellipse(x * 70, x * 20 + 100, x *10, 20);
 }
+  function draw() {
+  background(128, 0, 64);
+
+  ellipse(320, 240, mouseX, mouseY);
 }
